6140f23ea0f7a464fb3618feb44d85612010219f
egaragul <garagulya.eugen@gmail.com>
2021-06-18 20:05:20 +0300

    fix locale

--- src/main/kotlin/subtask1/DateFormatter.kt
+++ src/main/kotlin/subtask1/DateFormatter.kt
@@ -12,19 +12,12 @@ class DateFormatter {
     fun toTextDay(day: String, month: String, year: String): String {
         return try {
             val localDate = LocalDate.of(year.toInt(), month.toInt(), day.toInt())
-            val formatt = DateTimeFormatter.ofPattern("dd MMMM, EEEE", Locale.getDefault())
+            val formatt = DateTimeFormatter.ofPattern("dd MMMM, EEEE", Locale("ru", "RU"))
 
 
             formatt.format(localDate).toLowerCase()
         } catch (e: DateTimeException) {
             "Такого дня не существует"
         }
-
-
-//        val formatter = SimpleDateFormat("dd MMMM yyyy", Locale.getDefault())
-//
-//        val calendar = Calendar.getInstance()
-//        calendar.set(year.toInt(), month.toInt(), day.toInt())
-//        formatter.format(calendar.)
     }
 }
